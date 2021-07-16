1c0e7424dbbd88f68ad00d54fb7aa36697fedc12
Pedro Carvalho <pedrohutor@gmail.com>
2021-07-16 07:41:05 -0300

    dockerizaçao

--- BossaBox/settings.py
+++ BossaBox/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0b_@nlrit5szl8c483ldy&i4y&7ov9p9rk$=_dm2$scye1@k=2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
